d53534a30389172f0f437d5111a47b96fd8bb619
Zeno Gallegos <zenogallegos@Zeno-MacBook-Pro.local>
2024-02-01 17:14:05 -0700

    added algorithm folders

---
+++ m-3-Algorithms/01-arithmetic/Unsolved/arithmetic.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+
+add(10, 5)
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+subtract(10, 5)
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+multiply(10, 5)
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+divide(10, 5)
