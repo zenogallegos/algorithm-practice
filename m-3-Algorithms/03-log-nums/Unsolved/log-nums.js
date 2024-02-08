e1d7d6cd562a18e34e2c1fa8340463991c4f5ad3
Zeno Gallegos <zenogallegos@Zeno-MacBook-Pro.local>
2024-02-08 00:50:29 -0700

    finished module 4 algorithms

--- m-3-Algorithms/03-log-nums/Unsolved/log-nums.js
+++ m-3-Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -1,10 +1,8 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
-for (let i = 1; i < num; i++) {
-    
+var logNums = function (num) {
+  for (let i = 1; i <= num; i++) {
     console.log(i);
+  }
 };
-
-logNums(6);
--- m-4-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ m-4-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,12 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function(arr) {
+    let currentNum = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentArr = arr[i]
+        currentNum += currentArr;
+    }
+
+    return currentNum;
+};
